4e72467a034496b8f46b64e80cc2e976f1d643eb
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2018-12-24 16:00:09 +0200

    Initial commit

---
+++ src/main/java/com/chaika/batch/SpringBatchTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.chaika.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchTrainingApplication.class, args);
+	}
+
+}
+
