fb7f686c11d0cc9c0e9615a4bb84c30b627a9775
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2018-12-26 16:10:01 +0200

    Nested Job example

---
+++ src/main/java/com/chaika/batch/configuration/nested/ChildJobConfiguration.java
@@ -0,0 +1,43 @@
+package com.chaika.batch.configuration.nested;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by echaika on 26.12.2018
+ */
+@Configuration
+public class ChildJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public ChildJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step childJobStep1() {
+        return stepBuilderFactory.get("childJobStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> childJob Step 1");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job childJob() {
+        return jobBuilderFactory.get("childJob")
+                .start(childJobStep1())
+                .build();
+    }
+}
---
+++ src/main/java/com/chaika/batch/configuration/nested/ParentJobConfiguration.java
@@ -0,0 +1,65 @@
+package com.chaika.batch.configuration.nested;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.JobStepBuilder;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+/**
+ * Created by echaika on 26.12.2018
+ */
+@Configuration
+public class ParentJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private final Job childJob;
+
+    private final JobLauncher jobLauncher;
+
+    @Autowired
+    public ParentJobConfiguration(JobBuilderFactory jobBuilderFactory,
+                                  StepBuilderFactory stepBuilderFactory,
+                                  Job childJob,
+                                  JobLauncher jobLauncher) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.childJob = childJob;
+        this.jobLauncher = jobLauncher;
+    }
+
+    @Bean
+    public Step parentJobStep1() {
+        return stepBuilderFactory.get("parentJobStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> parentJob Step 1");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job parentJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        Step childJobStep = new JobStepBuilder(new StepBuilder("childJobStep"))
+                .job(childJob)
+                .launcher(jobLauncher)
+                .repository(jobRepository)
+                .transactionManager(transactionManager)
+                .build();
+
+        return jobBuilderFactory.get("parentJob")
+                .start(parentJobStep1())
+                .next(childJobStep)
+                .build();
+    }
+}
