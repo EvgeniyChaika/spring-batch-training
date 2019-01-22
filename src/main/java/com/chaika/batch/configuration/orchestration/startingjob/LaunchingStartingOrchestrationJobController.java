8ea3e3258d5a877d1199492f31eedd8f1e8f694b
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2019-01-22 11:54:33 +0200

    StartingOrchestrationJobConfiguration configuration

---
+++ src/main/java/com/chaika/batch/configuration/orchestration/startingjob/LaunchingStartingOrchestrationJobController.java
@@ -0,0 +1,46 @@
+package com.chaika.batch.configuration.orchestration.startingjob;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+/**
+ * Created by echaika on 22.01.2019
+ */
+@RestController
+public class LaunchingStartingOrchestrationJobController {
+
+    private final JobLauncher jobLauncher;
+
+    @Qualifier("startingOrchestrationJob")
+    private final Job startingOrchestrationJob;
+
+    private final JobOperator jobOperator;
+
+    @Autowired
+    public LaunchingStartingOrchestrationJobController(JobLauncher jobLauncher, Job startingOrchestrationJob, JobOperator jobOperator) {
+        this.jobLauncher = jobLauncher;
+        this.startingOrchestrationJob = startingOrchestrationJob;
+        this.jobOperator = jobOperator;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void launch(@RequestParam("name") String name) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", name)
+                .addDate("date", new Date())
+                .toJobParameters();
+
+//        jobLauncher.run(startingOrchestrationJob, jobParameters);
+        jobOperator.start("startingOrchestrationJob", String.format("name=%s, date=%s", name, new Date()));
+    }
+}
---
+++ src/main/java/com/chaika/batch/configuration/orchestration/startingjob/StartingOrchestrationJobConfiguration.java
@@ -0,0 +1,96 @@
+package com.chaika.batch.configuration.orchestration.startingjob;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.converter.DefaultJobParametersConverter;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.support.SimpleJobOperator;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by echaika on 22.01.2019
+ */
+@Configuration
+public class StartingOrchestrationJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private final JobExplorer jobExplorer;
+
+    private final JobRepository jobRepository;
+
+    private final JobRegistry jobRegistry;
+
+    private final JobLauncher jobLauncher;
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public StartingOrchestrationJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, JobExplorer jobExplorer, JobRepository jobRepository, JobRegistry jobRegistry, JobLauncher jobLauncher, ApplicationContext applicationContext) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.jobExplorer = jobExplorer;
+        this.jobRepository = jobRepository;
+        this.jobRegistry = jobRegistry;
+        this.jobLauncher = jobLauncher;
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor() throws Exception {
+        JobRegistryBeanPostProcessor registryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+
+        registryBeanPostProcessor.setJobRegistry(jobRegistry);
+        registryBeanPostProcessor.setBeanFactory(applicationContext.getAutowireCapableBeanFactory());
+        registryBeanPostProcessor.afterPropertiesSet();
+
+        return registryBeanPostProcessor;
+    }
+
+    @Bean
+    public JobOperator jobOperator() throws Exception {
+        SimpleJobOperator simpleJobOperator = new SimpleJobOperator();
+
+        simpleJobOperator.setJobLauncher(jobLauncher);
+        simpleJobOperator.setJobParametersConverter(new DefaultJobParametersConverter());
+        simpleJobOperator.setJobRepository(jobRepository);
+        simpleJobOperator.setJobExplorer(jobExplorer);
+        simpleJobOperator.setJobRegistry(jobRegistry);
+        simpleJobOperator.afterPropertiesSet();
+
+        return simpleJobOperator;
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet startingOrchestrationJobTasklet(@Value("#{jobParameters['name']}") String name) {
+        return (contribution, chunkContext) -> {
+            System.out.println(String.format("The job ran for %s", name));
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public Job startingOrchestrationJob() {
+        return jobBuilderFactory.get("startingOrchestrationJob")
+                .start(stepBuilderFactory.get("startingOrchestrationJobStep1")
+                        .tasklet(startingOrchestrationJobTasklet(null))
+                        .build())
+                .build();
+    }
+}
